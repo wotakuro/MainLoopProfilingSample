33d5ddeff46bc746d717d8c712734ba5ff46d3a7
Yusuke Kurokawa <wotakuro@gmail.com>
2018-08-03 22:53:45 +0900

    update

--- Assets/CustomPlayerLoop.cs
+++ Assets/CustomPlayerLoop.cs
@@ -8,96 +8,207 @@ using UnityEngine.Experimental.PlayerLoop;
 
 // プレイヤーループのカスタマイズ
 // 参考URL https://www.patreon.com/posts/unity-2018-1-16336053
-public class CustomPlayerLoop {
-
-    public class ProfilingUpdate
+namespace UTJ
+{
+    public class CustomPlayerLoop
     {
-        private float startTime;
-        private float executeTime;
-        public void Start()
-        {
-            this.startTime = Time.realtimeSinceStartup;
-        }
-        public void End()
-        {
-            executeTime = Time.realtimeSinceStartup - this.startTime;
-        }
-        public float GetExecuteTime()
-        {
-            return executeTime;
-        }
-    }
-    private static Dictionary<System.Type, ProfilingUpdate> profilingSubSystem;
 
-
-    [RuntimeInitializeOnLoadMethod]
-    static void Init()
-    {
-        System.Type[] profilePoints = {
-            typeof(PostLateUpdate.FinishFrameRendering),
-            typeof(Update.ScriptRunBehaviourUpdate),
-            typeof(PreLateUpdate.ScriptRunBehaviourLateUpdate),
-        };
-        var playerLoop = PlayerLoop.GetDefaultPlayerLoop();
-        AppendProfilingLoopSystem(ref playerLoop, profilePoints);
-        PlayerLoop.SetPlayerLoop(playerLoop);
-    }
-    public static float GetProfilingTime<T>()
-    {
-        return GetProfilingTime(typeof(T));
-    }
-    public static float GetProfilingTime(System.Type t)
-    {
-        ProfilingUpdate obj = null;
-        if( profilingSubSystem.TryGetValue(t, out obj))
+        public class ProfilingUpdate
         {
-            return obj.GetExecuteTime();
-        }
-        return 0.0f;
-    }
-
-    private static void AppendProfilingLoopSystem(ref PlayerLoopSystem playerLoop,System.Type[] profilePoints)
-    {
-        profilingSubSystem = new Dictionary<System.Type, ProfilingUpdate>();
-        // プロファイル対象のシステムを指定します
-        for( int i = 0; i < profilePoints.Length; ++i)
-        {
-            profilingSubSystem.Add(profilePoints[i], new ProfilingUpdate());
-
-        }
-        // Note: this also resets the loop to its defalt state first.        
-        var newSystems = new List<PlayerLoopSystem>();
-        for (int i = 0; i < playerLoop.subSystemList.Length; ++i)
-        {
-            var subSystem = playerLoop.subSystemList[i];
-
-            newSystems.Clear();
-            for (int j = 0; j < subSystem.subSystemList.Length; ++j)
+            private float startTime;
+            private float executeTime;
+            public void Start()
             {
-                var subsub = subSystem.subSystemList[j];
-                ProfilingUpdate updateObj;
-                if (profilingSubSystem.TryGetValue(subsub.type, out updateObj))
-                {
-                    // 実際のSystemとの間で挟みます
-                    newSystems.Add(new PlayerLoopSystem
-                    {
-                        type = typeof(ProfilingUpdate),
-                        updateDelegate = updateObj.Start
-                    });
-                    newSystems.Add(subsub);
-                    newSystems.Add(new PlayerLoopSystem
-                    {
-                        type = typeof(ProfilingUpdate),
-                        updateDelegate = updateObj.End
-                    });
-                }
-                else
-                {
-                    newSystems.Add(subsub);
-                }
+                this.startTime = Time.realtimeSinceStartup;
             }
-            subSystem.subSystemList = newSystems.ToArray();
-            playerLoop.subSystemList[i] = subSystem;
+            public void End()
+            {
+                // for physics.Update
+                executeTime += Time.realtimeSinceStartup - this.startTime;
+            }
+            public void Reset()
+            {
+                executeTime = 0.0f;
+            }
+            public float GetExecuteTime()
+            {
+                return executeTime;
+            }
+            public float GetStartTime()
+            {
+                return startTime;
+            }
+        }
+
+        private static Dictionary<System.Type, ProfilingUpdate> profilingSubSystem;
+        private static float loopStartTime;
+        //
+        private static Dictionary<System.Type, float> prevSubSystemExecuteTime;
+        private static float prevLoopExecuteTime;
+        private static float gfxWaitForPresentExecOnFinishRendering;
+
+        // culling Start point
+        private static float firstPreCullingPoint = 0.0f;
+
+
+        [RuntimeInitializeOnLoadMethod]
+        static void Init()
+        {
+            System.Type[] profilePoints = {
+                // script
+                typeof( Update.ScriptRunBehaviourUpdate),
+                typeof( PreLateUpdate.ScriptRunBehaviourLateUpdate),
+                typeof( FixedUpdate.ScriptRunBehaviourFixedUpdate),
+                // script (Coroutine)
+                typeof( Update.ScriptRunDelayedDynamicFrameRate),
+                // Animator
+                typeof(PreLateUpdate.DirectorUpdateAnimationBegin),
+                typeof(PreLateUpdate.DirectorUpdateAnimationEnd),
+                // Renderer
+                typeof(PostLateUpdate.UpdateAllRenderers),
+                typeof(PostLateUpdate.UpdateAllSkinnedMeshes),
+                // Rendering(require)
+                typeof(PostLateUpdate.FinishFrameRendering),
+                // Physics
+                typeof( FixedUpdate.PhysicsFixedUpdate),
+            };
+
+            var playerLoop = PlayerLoop.GetDefaultPlayerLoop();
+            AppendProfilingLoopSystem(ref playerLoop, profilePoints);
+            PlayerLoop.SetPlayerLoop(playerLoop);
+        }
+
+        public static float GetLastExecuteTime()
+        {
+            return prevLoopExecuteTime;
+        }
+        public static float GetGfxWaitForPresent()
+        {
+            return gfxWaitForPresentExecOnFinishRendering;
+        }
+
+        // For Android MultiThreadedRendering
+        public static void PreCulling()
+        {
+            if (firstPreCullingPoint == 0.0f)
+            {
+                firstPreCullingPoint = Time.realtimeSinceStartup;
+            }
+        }
+
+        public static float GetProfilingTime<T>()
+        {
+            return GetProfilingTime(typeof(T));
+        }
+        public static float GetProfilingTime(System.Type t)
+        {
+            float time = 0.0f;
+            if (prevSubSystemExecuteTime.TryGetValue(t, out time))
+            {
+                return time;
+            }
+            return 0.0f;
+        }
+
+        private static void AppendProfilingLoopSystem(ref PlayerLoopSystem playerLoop, System.Type[] profilePoints)
+        {
+            profilingSubSystem = new Dictionary<System.Type, ProfilingUpdate>();
+            prevSubSystemExecuteTime = new Dictionary<System.Type, float>();
+            // Add target subsytems
+            for (int i = 0; i < profilePoints.Length; ++i)
+            {
+                profilingSubSystem.Add(profilePoints[i], new ProfilingUpdate());
+                prevSubSystemExecuteTime.Add(profilePoints[i], 0.0f);
+            }
+
+            // FinishRendering is required point.
+            System.Type finishRenderingType = typeof(PostLateUpdate.FinishFrameRendering);
+            if (!profilingSubSystem.ContainsKey(finishRenderingType))
+            {
+                profilingSubSystem.Add(finishRenderingType, new ProfilingUpdate());
+                prevSubSystemExecuteTime.Add(finishRenderingType, 0.0f);
+            }
+
+
+            // Note: this also resets the loop to its defalt state first.        
+            var newSystems = new List<PlayerLoopSystem>();
+            for (int i = 0; i < playerLoop.subSystemList.Length; ++i)
+            {
+                var subSystem = playerLoop.subSystemList[i];
+
+                newSystems.Clear();
+                // skip Initialization / EarlyUpdate 
+                // Initialilzation.PlayerUpdateTime - WaitForTargetFPS ( win editor)
+                // EaryUpdate - (android -notmultithreaded)
+                if (i == 2)
+                {
+                    newSystems.Add(new PlayerLoopSystem
+                    {
+                        updateDelegate = Loop1stPoint
+                    });
+                }
+                for (int j = 0; j < subSystem.subSystemList.Length; ++j)
+                {
+                    var subsub = subSystem.subSystemList[j];
+                    ProfilingUpdate updateObj;
+                    if (profilingSubSystem.TryGetValue(subsub.type, out updateObj))
+                    {
+                        // SamplingStart - Exec - SamplingEnd
+                        newSystems.Add(new PlayerLoopSystem
+                        {
+                            type = typeof(ProfilingUpdate),
+                            updateDelegate = updateObj.Start
+                        });
+                        newSystems.Add(subsub);
+                        newSystems.Add(new PlayerLoopSystem
+                        {
+                            type = typeof(ProfilingUpdate),
+                            updateDelegate = updateObj.End
+                        });
+                    }
+                    else
+                    {
+                        newSystems.Add(subsub);
+                    }
+                }
+
+                if (i == playerLoop.subSystemList.Length - 1)
+                {
+                    newSystems.Add(new PlayerLoopSystem
+                    {
+                        updateDelegate = LoopLastPoint
+                    });
+                }
+                subSystem.subSystemList = newSystems.ToArray();
+                playerLoop.subSystemList[i] = subSystem;
+            }
+        }
+        private static void Loop1stPoint()
+        {
+            loopStartTime = Time.realtimeSinceStartup;
+        }
+
+        private static void LoopLastPoint()
+        {
+            float endTime = Time.realtimeSinceStartup;
+            foreach (var kv in profilingSubSystem)
+            {
+                prevSubSystemExecuteTime[kv.Key] = kv.Value.GetExecuteTime();
+                kv.Value.Reset();
+            }
+            prevLoopExecuteTime = endTime - loopStartTime;
+
+            if (firstPreCullingPoint != 0.0f)
+            {
+                float finishRenderingStart = profilingSubSystem[typeof(PostLateUpdate.FinishFrameRendering)].GetStartTime();
+                gfxWaitForPresentExecOnFinishRendering = firstPreCullingPoint - finishRenderingStart;
+            }
+            else
+            {
+                gfxWaitForPresentExecOnFinishRendering = 0.0f;
+            }
+            firstPreCullingPoint = 0.0f;
+//            SystemInfo.graphicsMultiThreaded
         }
     }
 }
--- Assets/ProfilingUI.cs
+++ Assets/ProfilingUI.cs
@@ -7,88 +7,78 @@ using UnityEngine.Profiling;
 using UnityEngine.Experimental.PlayerLoop;
 using System.Text;
 
-public class ProfilingUI : MonoBehaviour {
-    private Text text;
-    private StringBuilder stringBuilderBuffer = new StringBuilder();
-    private Recorder recordGfxWaitForPresent;
-
-    private Recorder finishRenderRecord;
-
-
-    private void Start()
+namespace UTJ
+{
+    public class ProfilingUI : MonoBehaviour
     {
-        text = this.GetComponent<Text>();
-        recordGfxWaitForPresent = Recorder.Get("Gfx.WaitForPresent");
+        private Text text;
+        private StringBuilder stringBuilderBuffer = new StringBuilder();
+        private Recorder recordGfxWaitForPresent;
+        private Recorder finishRenderRecord;
 
-        finishRenderRecord = Recorder.Get("PostLateUpdate.FinishFrameRendering");
-    }
-    // Update is called once per frame
-    void Update() {
-        if (Time.frameCount % 20 == 0)
+
+        private void Start()
         {
-            stringBuilderBuffer.Length = 0;
-            stringBuilderBuffer.Append("ScriptRunBehaviourUpdate:")
-                .AddMsecFromSec(CustomPlayerLoop.GetProfilingTime<Update.ScriptRunBehaviourUpdate>())
-                .Append("ms\n");
+            text = this.GetComponent<Text>();
+            recordGfxWaitForPresent = Recorder.Get("Gfx.WaitForPresent");
 
-            stringBuilderBuffer.Append("FinishFrameRendering:")
-                .AddMsecFromSec(CustomPlayerLoop.GetProfilingTime<PostLateUpdate.FinishFrameRendering>())
-                .Append("ms\n");
+            finishRenderRecord = Recorder.Get("PostLateUpdate.FinishFrameRendering");
+        }
+        // Update is called once per frame
+        void Update()
+        {
+            if (Time.frameCount % 20 == 0)
+            {
+                stringBuilderBuffer.Length = 0;
+                stringBuilderBuffer.Append("Exec:")
+                    .AddMsecFromSec(CustomPlayerLoop.GetLastExecuteTime())
+                    .Append("ms\n");
+
+                stringBuilderBuffer.Append("ScriptRunBehaviourUpdate:")
+                    .AddMsecFromSec(CustomPlayerLoop.GetProfilingTime<Update.ScriptRunBehaviourUpdate>())
+                    .Append("ms\n");
+
+                stringBuilderBuffer.Append("Update.ScriptRunDelayedDynamicFrameRate:")
+                    .AddMsecFromSec(CustomPlayerLoop.GetProfilingTime<Update.ScriptRunDelayedDynamicFrameRate>())
+                    .Append("ms\n");
 
 
 
-
-            stringBuilderBuffer.AddMsecFromNanosec(finishRenderRecord.elapsedNanoseconds).
-                Append("ms(").
-                Append(finishRenderRecord.sampleBlockCount);
-
-            stringBuilderBuffer.Append("\n");
-            stringBuilderBuffer.AddMsecFromNanosec(recordGfxWaitForPresent.elapsedNanoseconds);
-            stringBuilderBuffer.Append("ms(");
-            stringBuilderBuffer.Append(recordGfxWaitForPresent.sampleBlockCount);
-
-            text.text = stringBuilderBuffer.ToString();
+                text.text = stringBuilderBuffer.ToString();
+            }
         }
     }
-}
-public static class StringBuilderExtention { 
-    public static StringBuilder AddMsecFromSec(this StringBuilder sb, float tm)
+    public static class StringBuilderExtention
     {
-        const int num = 4;
-        int output = (int)(tm * 1000.0f * GetPow10(num));
-
-        sb.Append( output / GetPow10( num));
-        sb.Append(".");
-        for ( int i = 1; i < num; ++i)
+        public static StringBuilder AddMsecFromSec(this StringBuilder sb, float tm)
         {
-            int tmp = output / GetPow10( num   - i) % 10;
-            sb.Append(tmp);
+            const int num = 4;
+            int output = (int)(tm * 1000.0f * GetPow10(num));
+
+            sb.Append(output / GetPow10(num));
+            sb.Append(".");
+            for (int i = 1; i < num; ++i)
+            {
+                int tmp = output / GetPow10(num - i) % 10;
+                sb.Append(tmp);
+            }
+            return sb;
         }
-        return sb;
-    }
 
-    public static StringBuilder AddMsecFromNanosec(this StringBuilder sb,long nanosec)
-    {
-        const int num = 4;
-        int output = (int)(nanosec / GetPow10(8-num) );
-
-        sb.Append(output / GetPow10(num));
-
-        sb.Append(".");
-        for (int i = 1; i < num; ++i)
+        public static StringBuilder AddMsecFromNanosec(this StringBuilder sb, long nanosec)
         {
-            int tmp = output / GetPow10(num - i) % 10;
-            sb.Append(tmp);
+            float sec = (nanosec / 1000000000.0f);
+
+            return sb.AddMsecFromSec(sec);
         }
-        return sb;
-    }
-    private static int GetPow10(int p)
-    {
-        int param = 1;
-        for( int i =0;i<p; ++i)
+        private static int GetPow10(int p)
         {
-            param *= 10;
+            int param = 1;
+            for (int i = 0; i < p; ++i)
+            {
+                param *= 10;
+            }
+            return param;
         }
-        return param;
     }
 }
